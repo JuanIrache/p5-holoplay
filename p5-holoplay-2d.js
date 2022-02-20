ef2501e7d492452ca9b0edf79402505ca9562de9
JuanIrache <yrache@gmail.com>
2022-02-20 21:18:46 +0100

    refactor so that the module handles p5 sketch

--- p5-holoplay-2d.js
+++ p5-holoplay-2d.js
@@ -1,47 +1,8 @@
 import * as HoloPlayCore from './node_modules/holoplay-core/dist/holoplaycore.module.js';
-// import * as p5x from './node_modules/p5/lib/p5.js';
 
 import errors from './modules/errors.js';
 
 const worker = new Worker('./modules/quiltToPNG.worker.js');
-
-let client, specs, p, quilt, p5canvas, frameCount;
-let shapes = [];
-
-const create = async p5 => {
-  client = specs = quilt = p5canvas = null;
-  frameCount = 0;
-  shapes = [];
-  p = p5;
-  p.noLoop();
-  return new Promise((resolve, reject) => {
-    client = new HoloPlayCore.Client(
-      ({ devices }) => {
-        const device = devices[0];
-        if (!device) return reject('Device not found');
-        const {
-          quiltAspect: aspect,
-          quiltX: w,
-          quiltY: h,
-          tileX: vx,
-          tileY: vy
-        } = device.defaultQuilt;
-        specs = { vx, vy, vtotal: vx * vy, aspect };
-        p5canvas = p.createCanvas(w / vx, h / vy);
-        quilt = p.createGraphics(w, h);
-        worker.postMessage({
-          action: 'setSize',
-          payload: { size: [w, h] }
-        });
-        resolve(device);
-        p.draw();
-      },
-      e => reject(`HoloPlayCore Error code ${e.error}: ${errors[e.error]}`)
-    );
-  });
-};
-
-const add = (action, depth, options) => shapes.push({ action, depth, options });
 
 const drawView = ({ p, i, vtotal, shapes }) => {
   const viewProp = i / vtotal - 0.5;
@@ -55,47 +16,100 @@ const drawView = ({ p, i, vtotal, shapes }) => {
   }
 };
 
-const draw = async () => {
-  if (specs) {
-    shapes = shapes
-      .map(s => (s.depth === null ? { ...s, depth: Infinity } : s))
-      .sort((a, b) => b.depth - a.depth);
-    const { vx, vy, vtotal } = specs;
-    for (let y = 0; y < vy; y++) {
-      for (let x = 0; x < vx; x++) {
-        const i = y * vx + x;
-        drawView({ p, i, vtotal, shapes });
-        const quiltX = x * p.width;
-        const quiltY = p.height * vy - (y + 1) * p.height;
-        quilt.image(p5canvas, quiltX, quiltY);
-      }
+const drawQuilt = async ({
+  p,
+  client,
+  shapes,
+  specs,
+  quilt,
+  preview,
+  frameCount
+}) => {
+  shapes = shapes
+    .map(s => (s.depth === null ? { ...s, depth: Infinity } : s))
+    .sort((a, b) => b.depth - a.depth);
+  const { vx, vy, vtotal } = specs;
+  for (let y = 0; y < vy; y++) {
+    for (let x = 0; x < vx; x++) {
+      const i = y * vx + x;
+      drawView({ p, i, vtotal, shapes });
+      const quiltX = x * p.width;
+      const quiltY = p.height * vy - (y + 1) * p.height;
+      quilt.image(preview, quiltX, quiltY);
     }
-
-    worker.onmessage = ({ data }) => {
-      shapes = [];
-      p.draw();
-      client
-        .sendMessage(
-          new HoloPlayCore.ShowMessage(specs, new Uint8Array(data.payload))
-        )
-        .catch(e =>
-          console.error(
-            `HoloPlayCore Error code ${e.error}: ${errors[e.error]}`
-          )
-        );
-    };
-
-    const bitmap = await createImageBitmap(quilt.elt);
-
-    worker.postMessage({ action: 'saveFrame', payload: { bitmap } }, [bitmap]);
-    const i = Math.floor(vtotal / 2) + (frameCount % 2 === 0 ? 1 : -1);
-    drawView({ p, i, vtotal, shapes });
-    frameCount++;
   }
+
+  worker.onmessage = ({ data }) => {
+    p.draw();
+    client
+      .sendMessage(
+        new HoloPlayCore.ShowMessage(specs, new Uint8Array(data.payload))
+      )
+      .catch(e =>
+        console.error(`HoloPlayCore Error code ${e.error}: ${errors[e.error]}`)
+      );
+  };
+
+  const bitmap = await createImageBitmap(quilt.elt);
+
+  worker.postMessage({ action: 'saveFrame', payload: { bitmap } }, [bitmap]);
+  const i = Math.floor(vtotal / 2) + (frameCount % 2 === 0 ? 1 : -1);
+  drawView({ p, i, vtotal, shapes });
 };
 
-export default {
-  create,
-  add,
-  draw
+const promiseHoloPlayCore = () =>
+  new Promise((resolve, reject) => {
+    const client = new HoloPlayCore.Client(
+      ({ devices }) => {
+        const device = devices[0];
+        if (!device) reject('Device not found');
+
+        worker.postMessage({
+          action: 'setSize',
+          payload: {
+            size: [device.defaultQuilt.quiltX, device.defaultQuilt.quiltY]
+          }
+        });
+
+        resolve([client, device]);
+      },
+      e => reject(`Error code ${e.error} (${errors[e.error]})`)
+    );
+  });
+
+export default async ({ preload, setup, draw }) => {
+  const [client, device] = await promiseHoloPlayCore().catch(e =>
+    setup(null, null, `HoloPlayCore error: ${e.message}`)
+  );
+  const {
+    quiltAspect: aspect,
+    quiltX: w,
+    quiltY: h,
+    tileX: vx,
+    tileY: vy
+  } = device.defaultQuilt;
+  const specs = { vx, vy, vtotal: vx * vy, aspect };
+  const s = p => {
+    let preview, quilt;
+    let frameCount = 0;
+
+    if (preload) p.preload = () => preload(p);
+
+    p.setup = () => {
+      p.noLoop();
+      preview = p.createCanvas(w / vx, h / vy);
+      quilt = p.createGraphics(w, h);
+      setup(p, device);
+    };
+
+    p.draw = () => {
+      const shapes = [];
+      const add = (action, depth, options) =>
+        shapes.push({ action, depth, options });
+      draw(p, add);
+      drawQuilt({ p, client, shapes, specs, quilt, preview, frameCount });
+      frameCount++;
+    };
+  };
+  new p5(s);
 };
--- sketch.js
+++ sketch.js
@@ -1,41 +1,22 @@
-let canvas, context;
-const vx = 8;
-const vy = 6;
-const vtotal = vx * vy;
-const aspect = 0.75;
-const specs = { vx, vy, vtotal, aspect };
-const screen = 3360;
-const w = screen / vx;
-const h = screen / vy;
-
-const s = p => {
-  window = { ...window, ...p5 };
-  p.setup = () => {
-    P5Holoplay2d.create(p)
-      .then(device =>
-        console.log(
-          'Started HoloCore for device:',
-          device.hardwareVersion,
-          device.hwid
-        )
-      )
-      .catch(e => console.error('Error getting HoloCore started:', e));
-  };
-
-  p.draw = function () {
+const setup = (p, device, err) => {
+  if (err) console.error('Error getting HoloCore started:', e);
+  else {
+    console.log('Started HoloCore for', device.hardwareVersion, device.hwid);
     p.noStroke();
     p.textAlign(p.CENTER, p.CENTER);
     p.textSize(50);
-    P5Holoplay2d.add(() => p.background(250));
-    P5Holoplay2d.add(() => p.text('NEAR', p.width / 2, p.height * 0.75), -100);
-    P5Holoplay2d.add(() => p.text('CENTER', p.width / 2, p.height * 0.5), 0);
-    P5Holoplay2d.add(() => p.text('FAR', p.width / 2, p.height * 0.25), 100);
-    P5Holoplay2d.add(() => {
-      p.fill(255, 50, 50);
-      p.ellipse(p.width / 2, p.height / 2, 150);
-    }, 200 * Math.sin(p.millis() / 1000));
-    P5Holoplay2d.draw();
-  };
+  }
 };
 
-new p5(s);
+const draw = (p, add) => {
+  add(() => p.background(250));
+  add(() => p.text('NEAR', p.width / 2, p.height * 0.75), -100);
+  add(() => p.text('CENTER', p.width / 2, p.height * 0.5), 0);
+  add(() => p.text('FAR', p.width / 2, p.height * 0.25), 100);
+  add(() => {
+    p.fill(255, 50, 50);
+    p.ellipse(p.width / 2, p.height / 2, 150);
+  }, 200 * Math.sin(p.millis() / 1000));
+};
+
+setTimeout(() => P5Holoplay2d({ setup, draw }), 2000);
