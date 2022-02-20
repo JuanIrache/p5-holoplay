1f2c071b34d159371fa2481ab7e91125f48c7304
JuanIrache <yrache@gmail.com>
2022-02-21 00:16:27 +0100

    implement adaptable size and set true by default

--- p5-holoplay-2d.js
+++ p5-holoplay-2d.js
@@ -4,13 +4,22 @@ import errors from './modules/errors.js';
 
 const worker = new Worker('./modules/quiltToPNG.worker.js');
 
-const drawView = ({ p, i, vtotal, shapes }) => {
+const drawView = ({ p, i, vtotal, shapes, adaptSize }) => {
   const viewProp = i / vtotal - 0.5;
   for (const shape of shapes) {
     const maxDepthOff = 1 / 1 - -shape.depth;
     const depthOff = maxDepthOff * viewProp;
     p.push();
-    if (!isNaN(depthOff)) p.translate(depthOff, 0);
+    p.translate(shape.x, shape.y);
+    if (!isNaN(maxDepthOff)) {
+      if (adaptSize && shape.depth !== 0) {
+        const scaleAdjust = 1 / 2 ** (shape.depth / 200);
+        p.translate(p.width / 2, p.height / 2);
+        p.scale(scaleAdjust);
+        p.translate(-p.width / 2, -p.height / 2);
+      }
+      p.translate(depthOff, 0);
+    }
     shape.action();
     p.pop();
   }
@@ -24,6 +33,7 @@ const drawQuilt = async ({
   quilt,
   preview,
   frameCount,
+  adaptSize,
   wigglePreview
 }) => {
   shapes = shapes
@@ -33,7 +43,7 @@ const drawQuilt = async ({
   for (let y = 0; y < vy; y++) {
     for (let x = 0; x < vx; x++) {
       const i = y * vx + x;
-      drawView({ p, i, vtotal, shapes });
+      drawView({ p, i, vtotal, shapes, adaptSize });
       const quiltX = x * p.width;
       const quiltY = p.height * vy - (y + 1) * p.height;
       quilt.image(preview, quiltX, quiltY);
@@ -54,9 +64,10 @@ const drawQuilt = async ({
   const bitmap = await createImageBitmap(quilt.elt);
 
   worker.postMessage({ action: 'saveFrame', payload: { bitmap } }, [bitmap]);
+
   let i = Math.floor(vtotal / 2);
   if (wigglePreview) i += frameCount % 2 === 0 ? 1 : -1;
-  drawView({ p, i, vtotal, shapes });
+  drawView({ p, i, vtotal, shapes, adaptSize });
 };
 
 const promiseHoloPlayCore = () =>
@@ -80,11 +91,7 @@ const promiseHoloPlayCore = () =>
   });
 
 export default async ({ preload, setup, draw, options }) => {
-  const {
-    adjustSize = false,
-    adjustPosition = false,
-    wigglePreview = true
-  } = options || {};
+  const { adaptSize = true, wigglePreview = true } = options || {};
   const [client, device] = await promiseHoloPlayCore().catch(e =>
     setup(null, null, `HoloPlayCore error: ${e.message}`)
   );
@@ -122,6 +129,7 @@ export default async ({ preload, setup, draw, options }) => {
         quilt,
         preview,
         frameCount,
+        adaptSize,
         wigglePreview
       });
       frameCount++;
