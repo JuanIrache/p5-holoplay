72a37999e0e9c7e3ae12b5c5347a31beabf5b1ec
JuanIrache <yrache@gmail.com>
2022-02-21 20:32:34 +0100

    add mous interaction

--- sample-project/sketch.js
+++ sample-project/sketch.js
@@ -1,5 +1,13 @@
 const P5Holoplay2d = require('p5-holoplay-2d');
 
+const colors = [
+  [255, 50, 50],
+  [50, 155, 50],
+  [100, 100, 255]
+];
+
+let colorIdx = 0;
+
 const setup = (p, device, err) => {
   if (err) console.error(`Error getting HoloCore started: ${err}`);
   else {
@@ -7,6 +15,9 @@ const setup = (p, device, err) => {
     p.noStroke();
     p.textAlign(p.CENTER, p.CENTER);
     p.textSize(50);
+    p.mouseClicked = () => {
+      colorIdx = (colorIdx + 1) % colors.length;
+    };
   }
 };
 
@@ -17,7 +28,7 @@ const draw = (p, add) => {
   add(() => p.text('FAR', p.width / 2, p.height * 0.25), 100);
   add(() => {
     p.noStroke();
-    p.fill(255, 50, 50);
+    p.fill(...colors[colorIdx]);
     p.ellipse(p.width / 2, p.height / 2, 150);
   }, 200 * Math.sin(p.millis() / 600));
 };
